24098c57248e6e8bcc06880be1ef8c059cf1ba79
Kenandi Prince <kenandiprince98@gmail.com>
2021-10-31 00:07:50 +0100

    Organize landing page for home-page with text-slide

--- frontend-ng/src/app/components/home/home.component.ts
+++ frontend-ng/src/app/components/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  messages = ["Très empathique, facilement abordable et serviable",
+            "Capable de travail seul ou en équipe",
+            "Possedant une facilité à comminiqué avec mes co-workers",
+            "Riche en patience et très ouvert d'ésprit",
+            "Me considerant beaucoup plus comme un problem-solver qui utilise la programmation comme outil pour y arrivé",
+            "Possedant comme arme principale 'la créativité'",
+            "Capable de bien gérer mon temps et diviser mes tâches"]
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
